c6092d1f15be56a8743a68924695365f37ac4790
Dong Hur <dhur@boslogin04.rc.fas.harvard.edu>
2019-06-25 17:15:04 -0400

    working sbatch code

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 #SBATCH -J pfor
-#SBATCH -c 5 # Number of cores requested
+#SBATCH -c 8 # Number of cores requested
 #SBATCH -N 1 # Ensure that all cores are on one machine
 #SBATCH -t 30 # Runtime in minutes
 #SBATCH -p serial_requeue # Partition to submit to
-#SBATCH --mem=1000 # Memory per cpu in MB (see also --mem-per-cpu) 10GB
-#SBATCH --open-mode=append
-#SBATCH -o results/myoutput_46630138.out # Standard out goes to this file
-#SBATCH -e results/myerr_46630138.err # Standard err goes to this filehostname
-
-# Create a local work directory
-mkdir -p /scratch/$USER/46630138
-
-# Create results folder
-mkdir -p ~/BehaviorMapping/results/
+#SBATCH --mem=32G # Memory per cpu in MB (see also --mem-per-cpu) 10GB
+#SBATCH -o results/myoutput_%j.out # Standard out goes to this file
+#SBATCH -e results/myerr_%j.err # Standard err goes to this filehostname
 
 module load matlab/R2018b-fasrc01
-srun -c $SLURM_CPUS_PER_TASK matlab -nodisplay -nodesktop -nosplash <<EOF
-parpool('local', str2num(getenv('SLURM_CPUS_PER_TASK')));
-
-cd ~/BehaviorMapping/MotionMapper
-compile_mex_files
-
-addpath(genpath('~/BehaviorMapping'));
-cd ~/BehaviorMapping/MotionMapper
-runExample;
-save('../results/workspace_data.mat', 'embeddingValues');  
-
-delete(gcp);
-exit;
-EOF
-
-# Clean local work directory
-rm -rf /scratch/$USER/46630138
+srun -c $SLURM_CPUS_PER_TASK matlab -nodisplay -nodesktop -nosplash -r "run('MotionMapper/runExample.m'); save('results/workspace_data.mat', 'embeddingValues');  exit;"
