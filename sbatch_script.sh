a349028d1bd0e8e927d348655517f6c20a7ced17
Dong Hur <dhur@college.harvard.edu>
2019-07-01 14:51:56 -0400

    log

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -5,11 +5,11 @@
 #SBATCH -t 30 # Runtime in minutes
 #SBATCH -p serial_requeue # Partition to submit to
 #SBATCH --mem=10G # Memory per cpu in MB (see also --mem-per-cpu)
-#SBATCH -o results/myoutput_%j.out # Standard out goes to this file
-#SBATCH -e results/myerr_%j.err # Standard err goes to this filehostname
+#SBATCH -o log/%j/myoutput_%j.out # Standard out goes to this file
+#SBATCH -e log/%j/myerr_%j.err # Standard err goes to this filehostname
 
 
-module load Anaconda3/5.0.1-fasrc02
+module load Anaconda3/5..1-fasrc02
 python run.py
 
 module load matlab/R2018b-fasrc01
