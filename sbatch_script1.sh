2feca594d45357fe1e03e45d74058dbbaa268068
Dong Hur <ushur2016@gmail.com>
2019-07-02 22:59:46 -0400

    FINAL working code change

--- sbatch_script1.sh
+++ sbatch_script1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -c 2 # Number of cores requested
 #SBATCH -N 1 # Ensure that all cores are on one machine
-#SBATCH -t 30 # Runtime in minutes
+#SBATCH -t 45 # Runtime in minutes
 #SBATCH -p serial_requeue # Partition to submit to
-#SBATCH --mem=1G # Memory per cpu in MB (see also --mem-per-cpu)
+#SBATCH --mem=5G # Memory per cpu in MB (see also --mem-per-cpu)
 #SBATCH -o logs/myoutput_%j.out #Standard out goes to this file
 #SBATCH -e logs/myerr_%j.err # Standard err goes to this filehostname
 #SBATCH --mail-type=ALL
@@ -13,7 +13,7 @@
 # 8 cores need at least 10G
 
 module load Anaconda/5.0.1-fasrc02
-# conda create -n behavior --clone $PYTHON_HOME
+conda create -n behavior --clone $PYTHON_HOME
 source activate behavior
 python run.py
 source deactivate
